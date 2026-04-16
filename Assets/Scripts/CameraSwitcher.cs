ff202a1177f26de79166e1d7a33fb1c29e510263
Winkhashtag <winkhashtag@gmail.com>
2026-04-16 14:12:32 -0400

    cinemachien

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Cinemachine;
+public class CameraSwitcher : MonoBehaviour
+{ 
+  public Transform player;
+  public CinemachineCamera activeCam;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            activeCam.Priority = 1;
+        }
+    }
+
+        private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            activeCam.Priority = 0;
+        }
+    }
+}
+

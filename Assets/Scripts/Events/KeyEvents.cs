99eaaab8c312d75636d6a25496f55819714db577
beefstick-ht <ctrickel@pratt.edu>
2026-04-29 10:23:27 -0400

    clearing scripts

--- Assets/Scripts/Events/KeyEvents.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using UnityEngine.Events;
-
-public static class KeyEvents
-{
-    public static UnityAction<Key> onKeyCollected;
-}
--- Assets/Scripts/InteractionSystem/IInteractable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface IInteractable //is what is connecting all the other scripts to the interact function
-{
-    public string InteractionPrompt { get; }
-
-    public bool Interact (Interactor interactor);
-}
